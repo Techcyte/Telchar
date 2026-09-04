e653405ec1113e945f3cc0d215d0dae6968c61a0
Travis Taylor <travis.taylor@techcyte.com>
2026-09-04 15:42:49 -0600

    chore: fmt

--- crates/telchar/src/backend/local.rs
+++ crates/telchar/src/backend/local.rs
@@ -693,8 +693,8 @@ mod tests {
     use std::io;
 
     use super::{
-        MAXIMUM_BUILD_LOG_CHUNK_BYTES, MAXIMUM_QUEUED_BUILD_LOG_CHUNKS,
-        MAXIMUM_QUEUED_BUILD_LOG_PAYLOAD_BYTES, finish_logs, spawn_log_reader,
+        finish_logs, spawn_log_reader, MAXIMUM_BUILD_LOG_CHUNK_BYTES,
+        MAXIMUM_QUEUED_BUILD_LOG_CHUNKS, MAXIMUM_QUEUED_BUILD_LOG_PAYLOAD_BYTES,
     };
 
     struct FailingLogReader;
